703d72efadc9451fdf5df370d135ca7a1d818f3e
Thierry Santos <thierry.santos@skyone.solutions>
2020-05-02 19:56:53 -0300

    add trycatch

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const axios = require('axios');
+const axios = require("axios");
 
 (async function main() {
-  const response = await axios.get(
-    'https://api.github.com/users/thierrysantos'
-  );
-  console.log(JSON.stringify(response.data, null, 2));
+  try {
+    const response = await axios.get(
+      "https://api.github.com/users/thierrysantos"
+    );
+    console.log(JSON.stringify(response.data, null, 2));
+  } catch (error) {
+    console.log(JSON.stringify(error, null, 2));
+  }
 })();
