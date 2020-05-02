4e93a046cc195137a9dfce466fc53565f045ac1b
Thierry Santos <thierry.santos@skyone.solutions>
2020-05-02 16:43:28 -0300

    change github enpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 (async function main() {
   const response = await axios.get(
-    'https://api.github.com/users/thierrysantos/repos'
+    'https://api.github.com/users/thierrysantos'
   );
   console.log(response.data);
 })();
